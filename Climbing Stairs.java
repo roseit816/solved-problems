07fd0b994a0a76770b34145cf18f1aad520300ef
RAKSHITA ROSE K <roserakshita@gmail.com>
2026-03-26 00:13:10 +0530

    Create Climbing Stairs.java

---
+++ Climbing Stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+         if(n<=2) return n;
+        int prev2 = 1;
+        int prev1 = 2;
+        int ans = 0;
+        for(int state = 2; state < n; state++){
+            ans = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = ans;
+        }
+        return ans;
+    }
+}
