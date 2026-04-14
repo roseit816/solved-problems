851fa41a74abd076a5b97f853d5b63918879a5e8
RAKSHITA ROSE K <roserakshita@gmail.com>
2026-04-14 21:09:28 +0530

    Create Single Number.java

---
+++ Single Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int count = 0;
+
+        for (int num : nums) {
+            count ^= num;
+        }
+
+        return count;
+
+    }
+}
